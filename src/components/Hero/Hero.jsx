9237d05f797a0033e39df0f7994e74fa561eb32f
Julian-root <adjibiadechinan@gmail.com>
2024-07-20 00:00:09 +0100

    end feat hero banner

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import { FaWhatsapp } from 'react-icons/fa'
+import { UpdateFollower } from 'react-mouse-follower'
+import { delay, AnimatePresence, easeInOut, motion } from 'framer-motion'
+
+
 import Headphone1 from  '../../assets/headphones/headphone.png'
 import Headphone2 from  '../../assets/headphones/headphone-2.png'
 import Headphone3 from  '../../assets/headphones/headphone-3.png'
+
+const fadeUp = (delay) => {
+    return {
+        hidden: {
+            y: 100,
+            opacity: 0,
+            scale: 0.5
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            scale: 1,
+            transition: {
+                duration: 0.5,
+                delay: delay,
+                ease: easeInOut
+            }
+        },
+        exit : {
+            y: 50,
+            opacity: 0,
+            scale: 0.5,
+            transition: {
+                duration: 0.5,
+                ease: easeInOut
+            }
+        }
+    }
+}
 
 const HeadphoneData = [
     {
@@ -54,43 +87,119 @@ const Hero = () => {
                 xl:max-w-[500px]' >
 
                     <div className='space-y-5 text-center md:text-left' >
-                        <h1 className='text-3xl lg:text-6xl font-bold font-varela'>
-                            {activeData.title}
-                        </h1>
-                        <p className='text-sm leading-loose text-white/80'>
-                            {activeData.subtitle}
-                        </p>
-                        <button style={{ backgroundColor: activeData.bgColor }}
-                        className='px-4 py-2 inline-block rounded-sm font-normal'
-                        >
-                            Buy and Listen
-                        </button>
+                        
+                        <AnimatePresence mode="wait">
+
+                            <UpdateFollower
+                                    mouseOptions={
+                                    {
+                                        backgroundColor: 'white',
+                                        zIndex: '9999',
+                                        followSpeed: 0.5,
+                                        rotate: -720,
+                                        mixBlendMode: 'difference',
+                                        scale: 10
+                                    }
+                                }
+                            >
+                                <motion.h1 
+                                key={activeData.id}
+                                variants={fadeUp(0.2)}
+                                initial='hidden'
+                                animate='show'
+                                exit='exit'
+                                className='text-3xl lg:text-6xl font-bold font-varela'>
+                                    {activeData.title}
+                                </motion.h1>
+
+                            </UpdateFollower>
+                            
+                        </AnimatePresence>
+                        
+                        <AnimatePresence mode="wait">
+                            <motion.p 
+                            key={activeData.id}
+                            variants={fadeUp(0.3)}
+                            initial='hidden'
+                            animate='show'
+                            exit='exit'
+                            className='text-sm leading-loose text-white/80'>
+                                {activeData.subtitle}
+                            </motion.p>
+                        </AnimatePresence>
+
+                         
+                        <AnimatePresence mode="wait">
+                            <UpdateFollower
+                                    mouseOptions={
+                                    {
+                                        backgroundColor: activeData.bgColor,
+                                        zIndex: '9999',
+                                        followSpeed: 0.5,
+                                        rotate: -720,
+                                        mixBlendMode: 'difference',
+                                        scale: 10,
+                                        backgroundElement: (
+                                            <div>
+                                                <img src={activeData.image} alt="" />
+                                            </div>
+                                        ),
+                                    }
+                                }
+                            >
+
+                                <motion.button 
+                                key={activeData.id}
+                                variants={fadeUp(0.3)}
+                                initial='hidden'
+                                animate='show'
+                                exit='exit'
+                                style={{ backgroundColor: activeData.bgColor }}
+                                    className='px-4 py-2 inline-block rounded-sm font-normal'
+                                >
+                                Buy and Listen
+                                </motion.button>
+                            </UpdateFollower>
+                        </AnimatePresence>
 
                         {/* ________ HeadPhone List Separator ________ */}
-                        <div className='flex items-center justify-center md:justify-start gap-4 !mt-24'>
+                         <div className='flex items-center justify-center md:justify-start gap-4 !mt-24'>
                         </div>
 
                         {/* ________ HeadPhone List Switcher ________ */}
                         <div className='grid grid-cols-3 gap-10'>
                             {HeadphoneData.map((item) => {
                                 return (
-                                    <div 
-                                    key={item.id}
-                                    onClick={() => handleActiveData(item)}
-                                    className='grid grid-cols-2 place-items-center cursor-pointer' >
-                                        <div>
-                                            <img src={item.image} alt="" 
-                                            className='w-[200px]'/>
+                                    <UpdateFollower
+                                        mouseOptions={
+                                            {
+                                                backgroundColor: item.bgColor,
+                                                zIndex: '9999',
+                                                followSpeed: 0.5,
+                                                scale: 5,
+                                                text: "View Details",
+                                                textFontSize: '3px',
+                                            }
+                                        }
+                                    >
+                                        <div 
+                                        key={item.id}
+                                        onClick={() => handleActiveData(item)}
+                                        className='grid grid-cols-2 place-items-center cursor-pointer' >
+                                            <div>
+                                                <img src={item.image} alt="" 
+                                                className='w-[200px]'/>
+                                            </div>
+                                            <div className='space-y-2'>
+                                                <p className='text-base font-blod'>
+                                                    {item.price}
+                                                </p>
+                                                <p className='text-xs font-normal text-nowrap'>
+                                                    {item.modal}
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div className='space-y-2'>
-                                            <p className='text-base font-blod'>
-                                                {item.price}
-                                            </p>
-                                            <p className='text-xs font-normal text-nowrap'>
-                                                {item.modal}
-                                            </p>
-                                        </div>
-                                    </div>
+                                    </UpdateFollower>
                                 )
                             })}
                         </div>
@@ -101,8 +210,18 @@ const Hero = () => {
                 {/* ________ Hero Image ________ */}
 
                 <div className='flex flex-col justify-end items-center' >
-                    <img src={ activeData.image } alt=""
-                    className='w-[300px] md:w-[400px] xl:w-[550px]' />
+                    <AnimatePresence mode="wait">
+
+                    <motion.img 
+                        key={activeData.id}
+                        initial={{ opacity: 0, y: 100, scale: 0.9 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ duration: 0.4, delay: 0.2, ease: easeInOut }}
+                        exit= {{ opacity: 0, y: 100, scale: 0.9, transition: { duration: 0.2 } } }
+                        src={ activeData.image } alt=""
+                        className='w-[300px] md:w-[400px] xl:w-[550px]' />
+
+                    </AnimatePresence>
                 </div>
 
                 {/* ________ WhatsApp Icon ________ */}
